7cd289e7a326aacceb6194b66052814dcac7ca4f
Anna Ravna <ansvravna@gmail.com>
2024-12-08 18:24:45 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About'; // Ensure 'components' is spelled correctly
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import ThisWebsite from './components/ThisWebsite';
-import Footer from './components/Footer';
+import About from './components/About'; 
+import './App.css';
+// import Experience from './components/Experience';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
+// import ThisWebsite from './components/ThisWebsite';
+// import Footer from './components/Footer';
 
 
 function App() {
