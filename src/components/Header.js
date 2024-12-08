fba6a59e9f70b1a7b4bc1016d537c0b72ba71819
Anna Ravna <ansvravna@gmail.com>
2024-12-08 16:56:23 +0100

    First component header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './src/assets/logo3.webp'; 
+import logo from '../assets/logo3.webp'; 
 
 function Header() {
 
